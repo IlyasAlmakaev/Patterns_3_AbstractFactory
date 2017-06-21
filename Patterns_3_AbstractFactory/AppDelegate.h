de0ff0ed9c3589ba3c324fd45f6d75bf8c9a0a03
Ильяс <i.almakaev@gmail.com>
2017-06-22 05:31:40 +0700

    Initial Commit

---
+++ Patterns_3_AbstractFactory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns_3_AbstractFactory
+//
+//  Created by Ильяс on 22.06.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
